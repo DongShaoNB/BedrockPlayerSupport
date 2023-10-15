1f5b5fd214153a1963e695df823c5be3aa14db68
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2023-10-15 05:07:09 -0400

    增加自动登录成功提示, 并去除无用的判断

--- src/main/java/cn/dsnbo/bedrockplayersupport/listener/login/AuthMeListener.java
+++ src/main/java/cn/dsnbo/bedrockplayersupport/listener/login/AuthMeListener.java
@@ -2,6 +2,7 @@ package cn.dsnbo.bedrockplayersupport.listener.login;
 
 import cn.dsnbo.bedrockplayersupport.BedrockPlayerSupport;
 import fr.xephi.authme.api.v3.AuthMeApi;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,9 +19,8 @@ public class AuthMeListener implements Listener {
         if (FloodgateApi.getInstance().isFloodgatePlayer(player.getUniqueId())) {
             if (!AuthMeApi.getInstance().isAuthenticated(player)) {
                 if (AuthMeApi.getInstance().isRegistered(player.getName())) {
-                    if (BedrockPlayerSupport.getInstance().getConfig().getBoolean("login.enable")) {
-                        AuthMeApi.getInstance().forceLogin(player);
-                    }
+                    AuthMeApi.getInstance().forceLogin(player);
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', BedrockPlayerSupport.getInstance().getConfig().getString("login.auto-message")));
                 }
             }
         }
