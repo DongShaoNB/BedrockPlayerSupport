3359de0f0d42d04c0290d94e239861c85936c090
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2023-10-15 06:17:11 -0400

    增加CatSeedLogin和NexAuth的自动登录支持

---
+++ src/main/java/cn/dsnbo/bedrockplayersupport/listener/login/NexAuthListener.java
@@ -0,0 +1,28 @@
+package cn.dsnbo.bedrockplayersupport.listener.login;
+
+import cn.dsnbo.bedrockplayersupport.BedrockPlayerSupport;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.geysermc.floodgate.api.FloodgateApi;
+import su.nexmedia.auth.NexAuthAPI;
+import su.nexmedia.auth.auth.impl.AuthPlayer;
+import su.nexmedia.auth.auth.impl.PlayerState;
+
+/**
+ * @author DongShaoNB
+ */
+public class NexAuthListener implements Listener {
+    @EventHandler
+    public void onPlayerJoinEvent(PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+        if (FloodgateApi.getInstance().isFloodgatePlayer(player.getUniqueId())) {
+            if (AuthPlayer.getOrCreate(player).getState() != PlayerState.LOGGED_IN && AuthPlayer.getOrCreate(player).isRegistered()) {
+                NexAuthAPI.getAuthManager().login(player);
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', BedrockPlayerSupport.getInstance().getConfig().getString("login.auto-message")));
+            }
+        }
+    }
+}
