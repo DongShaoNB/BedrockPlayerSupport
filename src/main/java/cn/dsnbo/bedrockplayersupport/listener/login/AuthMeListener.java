e6a0d01ed8ea8a456d6f866268901832456a3a64
DongShaoNB <1964545962@qq.com>
2023-11-02 18:52:37 +0800

    [ci skip] back

--- src/main/java/cn/dsnbo/bedrockplayersupport/listener/login/AuthMeListener.java
+++ src/main/java/cn/dsnbo/bedrockplayersupport/listener/login/AuthMeListener.java
@@ -5,8 +5,6 @@ import cn.dsnbo.bedrockplayersupport.util.Forms;
 import fr.xephi.authme.api.v3.AuthMeApi;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.geysermc.floodgate.api.FloodgateApi;
@@ -17,7 +15,6 @@ import java.util.UUID;
  * @author DongShaoNB
  */
 public class AuthMeListener implements Listener {
-    @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerJoinEvent(PlayerJoinEvent e) {
         Player player = e.getPlayer();
         UUID playerUuid = player.getUniqueId();
@@ -27,9 +24,6 @@ public class AuthMeListener implements Listener {
                 if (AuthMeApi.getInstance().isRegistered(playerName)) {
                     AuthMeApi.getInstance().forceLogin(player);
                     player.sendMessage(ChatColor.translateAlternateColorCodes('&', BedrockPlayerSupport.getInstance().getConfig().getString("login.auto-message")));
-                } else {
-                    Forms.openRegisterForm(player);
-                    BedrockPlayerSupport.getInstance().getLogger().info("1");
                 }
             }
         }
