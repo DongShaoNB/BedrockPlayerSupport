e7935a3c30eed9b0cfac920902dd1a36060845e3
DongShaoNB <1964545962@qq.com>
2023-12-09 22:10:59 +0800

    修复使用AuthMe自动登录时不生效的BUG

--- src/main/java/cn/dsnbo/bedrockplayersupport/listener/login/AuthMeListener.java
+++ src/main/java/cn/dsnbo/bedrockplayersupport/listener/login/AuthMeListener.java
@@ -4,6 +4,7 @@ import cn.dsnbo.bedrockplayersupport.BedrockPlayerSupport;
 import fr.xephi.authme.api.v3.AuthMeApi;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.geysermc.floodgate.api.FloodgateApi;
@@ -14,8 +15,10 @@ import java.util.UUID;
  * @author DongShaoNB
  */
 public class AuthMeListener implements Listener {
-    public void onPlayerJoinEvent(PlayerJoinEvent e) {
-        Player player = e.getPlayer();
+
+    @EventHandler
+    public void onPlayerJoinEvent(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
         UUID playerUuid = player.getUniqueId();
         String playerName = player.getName();
         if (FloodgateApi.getInstance().isFloodgatePlayer(playerUuid)) {
