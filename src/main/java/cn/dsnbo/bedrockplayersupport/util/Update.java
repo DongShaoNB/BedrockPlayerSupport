cb017099b628f5aac5aa8a293220671e4372c0f0
HaHaWTH <id_cn00@outlook.com>
2024-01-30 19:45:33 +0800

    SpigotMC

--- src/main/java/cn/dsnbo/bedrockplayersupport/util/Update.java
+++ src/main/java/cn/dsnbo/bedrockplayersupport/util/Update.java
@@ -5,57 +5,33 @@ import com.tcoded.folialib.FoliaLib;
 import org.bukkit.Bukkit;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.Scanner;
+import java.util.function.Consumer;
 
 /**
  * @author DongShaoNB
  */
 public class Update {
-    private static final String currentVersion = BedrockPlayerSupport.getInstance().getDescription().getVersion();
-    public static void checkUpdate() {
+    private static final int spigotResourceId = 1145141919; // Spigot resource id
+    public static void checkUpdate(Consumer<String> consumer) {
         try {
             Class.forName("io.papermc.paper.threadedregions.RegionizedServer");
             FoliaLib foliaLib = new FoliaLib(BedrockPlayerSupport.getInstance());
-            foliaLib.getImpl().runAsync((task) -> checkUpdateV());
+            foliaLib.getImpl().runAsync((task) -> getVersion(consumer));
         } catch (ClassNotFoundException e) {
-            Bukkit.getScheduler().runTaskAsynchronously(BedrockPlayerSupport.getInstance(), Update::checkUpdateV);
+            Bukkit.getScheduler().runTaskAsynchronously(BedrockPlayerSupport.getInstance(), () -> getVersion(consumer));
         }
     }
 
-    public static void checkUpdateV() {
-        String owner = "DongShaoNB";
-        String repo = "BedrockPlayerSupport";
-        try {
-            URL url = new URL("https://api.github.com/repos/" + owner + "/" + repo + "/releases/latest");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setConnectTimeout(10000); // 设置连接超时为10秒
-            conn.setReadTimeout(10000); // 设置读取超时为10秒
-            Scanner scanner = new Scanner(conn.getInputStream());
-            String response = scanner.useDelimiter("\\Z").next();
-            scanner.close();
-            String latestVersion = response.substring(response.indexOf("tag_name") + 11);
-            latestVersion = latestVersion.substring(0, latestVersion.indexOf("\""));
-            if (isUpdateAvailable(latestVersion)) {
-                BedrockPlayerSupport.getInstance().getLogger().info("有新版本可以更新!");
-                BedrockPlayerSupport.getInstance().getLogger().info("当前版本: " + currentVersion + " | 最新版本: " + latestVersion);
-            } else {
-                BedrockPlayerSupport.getInstance().getLogger().info("插件是最新版本，继续保持哦~");
+    public static void getVersion(final Consumer<String> consumer) {
+        try (InputStream is = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + spigotResourceId + "/~").openStream(); Scanner scann = new Scanner(is)) {
+            if (scann.hasNext()) {
+                consumer.accept(scann.next());
             }
         } catch (IOException e) {
-            BedrockPlayerSupport.getInstance().getLogger().warning("无法检测更新，请检查网络情况");
+            BedrockPlayerSupport.getInstance().getLogger().info("Unable to check for updates: " + e.getMessage());
         }
-    }
-    private static boolean isUpdateAvailable(String latestVersion) {
-        String[] latestVersionArray = latestVersion.substring(1).split("\\.");
-        String[] currentVersionArray = currentVersion.split("\\.");
-        for (int i = 0; i < Math.min(currentVersionArray.length, latestVersionArray.length); i++) {
-            int currentPart = Integer.parseInt(currentVersionArray[i]);
-            int latestPart = Integer.parseInt(latestVersionArray[i]);
-            if (latestPart > currentPart) return true;
-        }
-        return false;
-    }
-}
+    }}
 
