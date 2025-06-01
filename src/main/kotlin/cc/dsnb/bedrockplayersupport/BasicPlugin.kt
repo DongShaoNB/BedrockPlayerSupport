bcb6ec0d3833de37fee22fe67c98f7277d76f867
DongShaoNB <54093566+DongShaoNB@users.noreply.github.com>
2025-06-01 12:39:08 +0800

    refactor(BasicPlugin): simplify BasicPlugin enum declaration
    - Remove redundant pluginName parameter
    - Set default values for tpaCommand, tpaHereCommand, tpaAcceptCommand, and tpaRejectCommand
    - Update enum values to use default parameters where applicable
    - Adjust specific enum values to override default parameters when needed

--- src/main/kotlin/cc/dsnb/bedrockplayersupport/BasicPlugin.kt
+++ src/main/kotlin/cc/dsnb/bedrockplayersupport/BasicPlugin.kt
@@ -4,18 +4,18 @@ package cc.dsnb.bedrockplayersupport
  * @author DongShaoNB
  */
 enum class BasicPlugin(
-    val pluginName: String,
-    val tpaCommand: String,
-    val tpaHereCommand: String,
-    val tpaAcceptCommand: String,
-    val tpaRejectCommand: String
+    val pluginName: String = "",
+    val tpaCommand: String = "/tpa",
+    val tpaHereCommand: String = "/tpahere",
+    val tpaAcceptCommand: String = "/tpaccept",
+    val tpaRejectCommand: String = "/tpdeny"
 ) {
 
-    CMI("CMI", "/tpa", "/tpahere", "/tpaccept", "/tpdeny"),
-    ESSENTIALS("EssentialsX", "/tpa", "/tpahere", "/tpaccept", "/tpdeny"),
-    HUSKHOMES("HuskHomes", "/tpa", "/tpahere", "/tpaccept", "/tpdecline"),
-    ADVANCEDTELEPORT("AdvancedTeleport", "/tpa", "/tpahere", "/tpayes", "/tpano"),
+    CMI("CMI"),
+    ESSENTIALS("EssentialsX"),
+    HUSKHOMES("HuskHomes", tpaRejectCommand = "/tpdecline"),
+    ADVANCEDTELEPORT("AdvancedTeleport", tpaAcceptCommand = "/tpayes", tpaRejectCommand = "/tpano"),
     SUNLIGHT("SunLight", "/ptp request", "/ptp invite", "/ptp accept", "/ptp decline"),
-    NONE("None", "None", "None", "None", "None");
+    NONE;
 
 }
